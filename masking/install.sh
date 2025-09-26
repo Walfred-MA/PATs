cf18c25cf81a4227d0c73412a55c56a1a5af0276
Walfred-MA <90290821+Walfred-MA@users.noreply.github.com>
2025-09-26 10:14:35 -0700

    Create install.sh

---
+++ masking/install.sh
@@ -0,0 +1,5 @@
+# install snakemake 6.15.0 into current environment
+conda install -c bioconda -c conda-forge snakemake==6.15.0
+
+# install blast into current environment
+conda install -c bioconda -c conda-forge blast
