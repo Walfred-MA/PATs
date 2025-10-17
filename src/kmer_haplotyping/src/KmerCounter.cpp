b8e2574e254d2effba9259380a3e0023afcf81f0
Walfred-MA <90290821+Walfred-MA@users.noreply.github.com>
2025-10-16 22:35:45 -0400

    Delete src/kmer_haplotyping directory

--- src/kmer_haplotyping/src/KmerCounter.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  KmerCounter.cpp
-//  kmer_haplotyping
-//
-//  Created by Wangfei MA on 2/2/23.
-//  Copyright © 2023 USC_Mark. All rights reserved.
-//
-
-#include "KmerCounter.hpp"
--- src/kmer_haplotyping/src/KmerFilter.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  KmerFilter.cpp
-//  merge_kmerlist
-//
-//  Created by Wangfei MA on 9/6/23.
-//
-
-#include "KmerFilter.hpp"
